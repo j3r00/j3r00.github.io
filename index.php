a2c71d0c97045253ef0273d037e28deac4083367
ZackenBaron <nope>
2020-05-01 16:33:22 +0200

    Changed index.html to index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ZackenBaron's Page :: Menu</title>
+  </head>
+  <body>
+    <center>
+      <h1>Huch</h1>
+      <p>Hier ist noch nichts</p>
+      </center>
+  </body>
+</html>
